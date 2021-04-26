babd70b499a664c1037605def9456305db99d333
Trey Kemp <kemptrey@outlook.com>
2021-04-26 12:32:30 -0600

    styling

--- src/Footer.js
+++ src/Footer.js
@@ -5,13 +5,34 @@ const Footer = (props) => {
   const setFilter = (filter) => {
     props.dispatch({ type: "SET_FILTER", filter: filter });
   };
+
+  const getStyle = (filter) => {
+    if (props.filter === filter) {
+      return { border: "3px solid" };
+    } else {
+      return {};
+    }
+  };
   return (
     <div>
-      <div onClick={() => setFilter("all")}>All</div>
-      <div onClick={() => setFilter("completed")}>completed</div>
-      <div onClick={() => setFilter("incomplete")}>incomplete</div>
+      <div style={getStyle("All")} onClick={() => setFilter("All")}>
+        All
+      </div>
+      <div style={getStyle("Completed")} onClick={() => setFilter("Completed")}>
+        Completed
+      </div>
+      <div
+        style={getStyle("Incomplete")}
+        onClick={() => setFilter("Incomplete")}
+      >
+        Incomplete
+      </div>
     </div>
   );
 };
 
-export default connect()(Footer);
+const mapStateToProps = (state) => {
+  return { filter: state.filter };
+};
+
+export default connect(mapStateToProps)(Footer);
